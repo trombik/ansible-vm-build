ab78e92286654e622ff1e23363a016a7afe91cad
Tomoyuki Sakurai <y@trombik.org>
2022-02-02 17:52:26 +0700

    feat: add OpenBSD 7.0

---
+++ OpenBSD-7.0/scripts/init.sh
@@ -0,0 +1,55 @@
+#!/bin/ksh
+
+set -e
+set -x
+
+python_version="3.8"
+case `uname -r` in
+    6.5)
+        python_version="3.6"
+        ;;
+    6.8)
+        python_version="3.7"
+        ;;
+esac
+python_package="python%${python_version}"
+
+sudo pkg_add ${python_package} ansible rsync-- curl
+sudo ln -sf /usr/local/bin/python${python_version} /usr/local/bin/python
+sudo ln -sf /usr/local/bin/python${python_version}-config /usr/local/bin/python-config
+sudo ln -sf /usr/local/bin/pydoc${python_version}  /usr/local/bin/pydoc
+
+# install latest ansible on older versions
+case `uname -r` in
+7.*)
+    # ssh-rsa has been disabled since 7.0
+    echo PubkeyAcceptedKeyTypes +ssh-rsa | sudo tee -a  /etc/ssh/sshd_config
+    ;;
+esac
+
+sudo tee /etc/rc.conf.local <<EOF
+sndiod_flags=NO
+sendmail_flags=NO
+EOF
+
+sudo sed -i'.bak' -e 's/ \/opt ffs rw,nodev,nosuid 1 2/ \/opt ffs rw,nosuid 1 2/' /etc/fstab
+sudo rm /etc/fstab.bak
+
+sudo sed -i'.bak' -e 's/\(ttyC[^0].*getty.*\)on /\1off/' /etc/ttys
+sudo rm /etc/ttys.bak
+
+if sysctl -n kern.version | head -n1 | grep -q -- -current ;then
+    # syspatch is not available for -current
+    :
+else
+    # $? == 2 means no patch available
+    if ! sudo syspatch && [ $? -ne 2 ]; then
+        exit $?
+    fi
+    # run syspatch again. when syspatch has been updated, you need to run it
+    # again.
+    # "syspatch updated itself, run it again to install missing patches"
+    if ! sudo syspatch && [ $? -ne 2 ]; then
+        exit $?
+    fi
+fi
