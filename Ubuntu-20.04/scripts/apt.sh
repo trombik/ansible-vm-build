b45b7516b71547cf05ff5799451550f8ea68d83a
Tomoyuki Sakurai <y@trombik.org>
2022-02-07 13:03:48 +0700

    feat: add Ubuntu 20.04

---
+++ Ubuntu-20.04/scripts/apt.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# In Ubuntu 12.04, the contents of /var/lib/apt/lists are corrupt
+ubuntu_version=$(lsb_release -r | awk '{ print $2 }')
+if [ "$ubuntu_version" == '12.04' ]; then
+  sudo rm -rf /var/lib/apt/lists
+fi
+
+# Disable periodic activities of apt, which causes `apt` tasks to fail by
+# holding a lock
+if [ "$ubuntu_version" != '14.04' ]; then
+  sudo -S tee -a /etc/apt/apt.conf.d/10disable-periodic <<EOF
+APT::Periodic::Enable "0";
+EOF
+fi
+
+# Retry when fetching files fails
+sudo -S tee -a /etc/apt/apt.conf.d/10retry <<EOF
+Acquire::Retries "10";
+EOF
+
+sudo apt-get update
+sudo apt-get -y install software-properties-common
+
+source /etc/os-release
+if [ "${NAME}" == "Devuan GNU/Linux" ]; then
+    # XXX this should work, but does not
+    # sudo apt-add-repository "deb http://deb.devuan.org/merged beowulf-backports main"
+    echo "deb http://deb.devuan.org/merged ${VERSION_CODENAME}-backports main" | sudo tee -a /etc/apt/sources.list
+else
+    # install the latest ansible from ppa
+    sudo apt-add-repository ppa:ansible/ansible-2.10
+fi
+
+sudo apt-get update
+if [ "${NAME}" == "Devuan GNU/Linux" ]; then
+    sudo apt-get -y -t ${VERSION_CODENAME}-backports install python3 ansible rsync
+else
+    sudo apt-get -y install python3 ansible rsync
+fi
