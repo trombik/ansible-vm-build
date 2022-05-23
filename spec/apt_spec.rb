28d43e0a98cccaf59281dead758b4a9b50566ae4
Tomoyuki Sakurai <y@trombik.org>
2022-05-23 17:08:23 +0700

    bugfix: add debian and devuan to apt spec
    remove `>= 16.04` guard clause because we do not maintain 16.x

--- spec/apt_spec.rb
+++ spec/apt_spec.rb
@@ -3,15 +3,14 @@
 require_relative "spec_helper"
 
 case os[:family]
-when "ubuntu"
-  if os[:release].to_f >= 16.04
-    describe file("/etc/apt/apt.conf.d/10disable-periodic") do
-      it { should be_exist }
-      it { should be_file }
-      it { should be_mode 644 }
-      its(:content) { should match(/^APT::Periodic::Enable\s+"0";/) }
-    end
+when "ubuntu", "debian", "devuan"
+  describe file("/etc/apt/apt.conf.d/10disable-periodic") do
+    it { should be_exist }
+    it { should be_file }
+    it { should be_mode 644 }
+    its(:content) { should match(/^APT::Periodic::Enable\s+"0";/) }
   end
+
   describe file("/etc/apt/apt.conf.d/10retry") do
     it { should be_exist }
     it { should be_file }
@@ -19,7 +18,7 @@ when "ubuntu"
     its(:content) { should match(/^Acquire::Retries\s+"10";/) }
   end
 
-  if os[:release].to_f >= 20.04
+  if os[:family] == "ubuntu" && os[:release].to_f >= 20.04
     describe command("dpkg -l") do
       its(:exit_status) { should eq 0 }
       its(:stdout) { should_not match(/^\S+\s+snapd\s+/) }
